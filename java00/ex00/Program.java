623eb5b7527fc4dbc812b5e3e37d38402e42e1ae
Mohammed Ouerchi <mouerchi@e3r2p5.1337.ma>
2025-08-05 20:27:54 +0100

    java00-ex00

---
+++ java00/ex00/Program.java
@@ -0,0 +1,16 @@
+
+
+
+
+class Program{
+	public static void main(String[] args) {
+		int num = 479598;
+		int result = 0;
+
+		while (num > 0){
+			result += num % 10;
+			num /= 10;
+		}
+		System.out.println(result);
+	}
+}
