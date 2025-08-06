d16165e4cae64021c74e7ba71df4317fd6749049
Mohammed Ouerchi <mouerchi@e3r2p5.1337.ma>
2025-08-06 02:01:38 +0100

    ex01

---
+++ java00/ex01/Program.java
@@ -0,0 +1,43 @@
+
+import java.util.Scanner;
+
+
+class Program{
+
+	public static boolean isInteger(Scanner scanner){
+		return scanner.hasNextInt();
+	}
+
+	public static void isPrime(int input){
+		int steps = 1;
+		for (int i = 2; i * i <= input; i++){
+			if (input % i == 0){
+				System.out.println("false" + " " + steps);
+				return;
+			}
+			steps++;
+		}
+		System.out.println("true" + " " + steps);
+	}
+
+	public static boolean isValidInput(int input){
+		return input >= 2;
+	}
+	public static void main(String[] args){
+		Scanner scanner = new Scanner(System.in);
+		System.out.print("-> ");
+		if (!isInteger(scanner)){
+			System.out.println("Invalid Integer");
+			scanner.close();
+			System.exit(0);
+		}
+		int input = scanner.nextInt();
+		if (!isValidInput(input)){
+			System.out.println("IllegalArgument");
+			scanner.close();
+			return;
+		}
+		isPrime(input);
+		scanner.close();
+	}
+}
